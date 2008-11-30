a88d3e980da3d4f5b9b6ac4641daabaa8fab26d5
sun <sun@unleashedmind.com>
2008-11-30 17:16:42 +0000

    #328052 by sun: Fixed switching input formats leads to wrong editor/state.

--- wysiwyg.init.js
+++ wysiwyg.init.js
@@ -2,7 +2,7 @@
 
 Drupal.behaviors = Drupal.behaviors || {}; // D5 only.
 
-Drupal.wysiwyg = Drupal.wysiwyg || {};
+Drupal.wysiwyg = Drupal.wysiwyg || { 'instances': {} };
 
 Drupal.wysiwyg.editor = Drupal.wysiwyg.editor || { 'init': {}, 'attach': {}, 'detach': {} };
 
