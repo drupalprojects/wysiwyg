49920cf0e5b4bdb7ac80ab5eb93908390b68cf23
Henrik Danielsson <h.danielsson@gmail.com>
2011-02-09 13:40:24 +0100

    Moved the recursive DOM code to its own function, no errors now but needs IE outerHTML workaround.

--- wysiwyg.init.js
+++ wysiwyg.init.js
@@ -19,53 +19,56 @@ Drupal.wysiwyg.plugins = Drupal.wysiwyg.plugins || {};
     $.support.queryCommandEnabled = true;
   }
 
-  $.fn.extend({
-    xhtml : function (value) {
-      var node = this[0];
-      // (v0.4) Written 2006 by Steve Tucker, http://www.stevetucker.co.uk
-      if (!node || node.nodeType != 1) {return false;}
-      var children = node.childNodes;
-      var xhtmlContent = '';
-      for (var i=0; i< children.length; i++) {
-        var child = children[i];
-        if (child.nodeType == 3) {
-          // Text.
-          xhtmlContent += child.nodeValue.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+  function _xhtml(node) {
+    // (v0.4) Written 2006 by Steve Tucker, http://www.stevetucker.co.uk
+    if (!node || node.nodeType != 1) {return '';}
+    var children = node.childNodes;
+    var xhtmlContent = '';
+    for (var i=0; i< children.length; i++) {
+      var child = children[i];
+      if (child.nodeType == 3) {
+        // Text.
+        xhtmlContent += child.nodeValue.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+      }
+      else if (child.nodeType == 8) {
+        // Comment.
+        xhtmlContent += '<!--' + child.nodeValue + '-->';
+      }
+      else {
+        xhtmlContent += '<' + child.nodeName.toLowerCase();
+        var attributes = child.attributes;
+        for (var j=0; j < attributes.length; j++) {
+          var attribute = attributes[j];
+          var attName = attribute.nodeName.toLowerCase();
+          var attValue = attribute.nodeValue;
+          if (attName == 'style' && child.style.cssText) {
+            // Todo: not very nice way to handle styles.
+            xhtmlContent += ' style="' + child.style.cssText.toLowerCase() + '"';
+          }
+          else if (attValue && attName != 'contenteditable') {
+            xhtmlContent += ' ' + attName + '="' + attValue + '"';
+          }
         }
-        else if (child.nodeType == 8) {
-          // Comment.
-          xhtmlContent += '<!--' + child.nodeValue + '-->';
+        var tagName = child.nodeName.toLowerCase();
+        var innerContent = _xhtml(child);
+        var elemClone = child.cloneNode(true);
+        var container = document.createElement('div');
+        container.appendChild(elemClone);
+        if (!new RegExp('</' + tagName + '>\s*$', 'i').test(container.innerHTML)) {
+          xhtmlContent += ' />' + innerContent;
         }
         else {
-          xhtmlContent += '<' + child.nodeName.toLowerCase();
-          var attributes = child.attributes;
-          for (var j=0; j < attributes.length; j++) {
-            var attribute = attributes[j];
-            var attName = attribute.nodeName.toLowerCase();
-            var attValue = attribute.nodeValue;
-            if (attName == 'style' && child.style.cssText) {
-              // Todo: not very nice way to handle styles.
-              xhtmlContent += ' style="' + child.style.cssText.toLowerCase() + '"';
-            }
-            else if (attValue && attName != 'contenteditable') {
-              xhtmlContent += ' ' + attName + '="' + attValue + '"';
-            }
-          }
-          var tagName = child.nodeName.toLowerCase();
-          var innerContent = $.fn.xhtml.apply(child);
-          var elemClone = child.cloneNode(true);
-          var container = document.createElement('div');
-          container.appendChild(elemClone);
-          if (!new RegExp('</' + tagName + '>\s*$', 'i').test(container.innerHTML)) {
-            xhtmlContent += ' />' + innerContent;
-          }
-          else {
-            xhtmlContent += '>' + innerContent + '</' + tagName + '>';
-          }
-          delete container;
+          xhtmlContent += '>' + innerContent + '</' + tagName + '>';
         }
+        delete container;
       }
-      return xhtmlContent;
+    }
+    return xhtmlContent;
+  }
+
+  $.fn.extend({
+    xhtml : function (value) {
+      return _xhtml(this[0]);
     }
   });
 })(jQuery);
