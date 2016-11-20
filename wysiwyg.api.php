8339acab7dcca46ce3fc8d1d17f447aeb6705f46
jenna.tollerson <jenna.tollerson@147099.no-reply.drupal.org>
2016-11-20 13:09:51 +0100

    - #2821817 by jenna.tollerson: Added CKEditor specific note to plugin documentation.

--- wysiwyg.api.php
+++ wysiwyg.api.php
@@ -53,6 +53,8 @@ function hook_wysiwyg_plugin($editor, $version) {
             // A list of buttons provided by this native plugin. The key has to
             // match the corresponding JavaScript implementation. The value is
             // is displayed on the editor configuration form only.
+            // CKEditor-specific note: The internal button name/key is
+            // capitalized, i.e. Img_assist.
             'buttons' => array(
               'img_assist' => t('Image Assist'),
             ),
